ad052931b2897404156a1d4d24c2ee5fb2dffdd3
sharmaprachi1504 <99645104+sharmaprachi1504@users.noreply.github.com>
2023-09-15 14:34:38 +0530

    Update providers.tf

--- Bench_Training/providers.tf
+++ Bench_Training/providers.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = var.AWS_REGION
-  #aws = {
-  #source  = "hashicorp/aws"
-  #version = "~> 2.0"
-  #}
+  aws = {
+  source  = "hashicorp/aws"
+  version = "~> 2.0"
+  }
 }
