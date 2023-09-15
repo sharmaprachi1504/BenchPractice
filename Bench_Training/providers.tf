9eef7befa4bebe3dc6830f60887bfd0e4d3bca02
sharmaprachi1504 <99645104+sharmaprachi1504@users.noreply.github.com>
2023-09-15 14:41:37 +0530

    Update providers.tf

--- Bench_Training/providers.tf
+++ Bench_Training/providers.tf
@@ -1,7 +1,17 @@
-provider "aws" {
-  region = var.AWS_REGION
-  aws = {
-  source  = "hashicorp/aws"
-  version = "~> 2.0"
+# provider "aws" {
+ # region = var.AWS_REGION
+  # aws = {
+  # source  = "hashicorp/aws"
+ # version = "~> 2.0"
+ # }
+# } 
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
   }
 }
